8665fb2dd52e19cf534917f9cd4fb78162defebb
suhejb <suhejbmorina@gmail.com>
2025-09-30 09:35:48 +0200

    try one more time

--- index.js
+++ index.js
@@ -64,5 +64,6 @@ app.get("/qr", async (req, res) => {
     res.json(data);
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
